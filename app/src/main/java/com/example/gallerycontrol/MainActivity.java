f1df5befce3cf2e115c50b53449df1cf784c5336
SANDHYA <115525118+sandhyabalamurali@users.noreply.github.com>
2024-04-21 18:27:25 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/gallerycontrol/MainActivity.java
+++ app/src/main/java/com/example/gallerycontrol/MainActivity.java
@@ -1,14 +1,66 @@
-package com.example.gallerycontrol;
+package com.example.gallerycontol;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private Integer[] mImageIds = {
+            R.drawable.image1,
+            R.drawable.image2,
+            R.drawable.image3,
+            R.drawable.image4
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Gallery gallery = (Gallery) findViewById(R.id.gallery);
+        gallery.setAdapter(new ImageAdapter(this));
+
+        gallery.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ImageView imageView = (ImageView) findViewById(R.id.imageView);
+                imageView.setImageResource(mImageIds[position]);
+            }
+        });
+    }
+
+    public class ImageAdapter extends BaseAdapter {
+        private Context mContext;
+
+        public ImageAdapter(Context c) {
+            mContext = c;
+        }
+
+        public int getCount() {
+            return mImageIds.length;
+        }
+
+        public Object getItem(int position) {
+            return null;
+        }
+
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView = new ImageView(mContext);
+            imageView.setImageResource(mImageIds[position]);
+            imageView.setLayoutParams(new Gallery.LayoutParams(150, 100));
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            return imageView;
+        }
     }
 }
